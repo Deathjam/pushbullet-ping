0cf7c2bdd74fe2a46f73b1d04e042d8d485c80fb
Phil <philwray@gmail.com>
2013-09-16 22:38:22 +0100

    Update pb-ping.sh

--- pb-ping.sh
+++ pb-ping.sh
@@ -3,6 +3,7 @@
 # Author: Phil Wray
 # Sign up at: https://www.pushbullet.com/
 # Enter your api and device_id below
+# Add script to cron to make sure always running
 
 SERVER=www.google.com #SERVER TO PING
 URL=https://www.pushbullet.com/api/pushes #PUSHBULLET API URL
@@ -11,7 +12,18 @@ DEV= #DEVICE ID
 HEADING="Houston we have a problem" #NOTE TITLE
 MESSAGE="Everything is on fire and the world is ending, oh noes" #NOTE BODY
 SLEEP=120 #Sleep time in seconds
+LOCKFILE=/tmp/pb-ping.lock
 
+#Create lock file
+if [ -e ${LOCKFILE} ] && kill -0 `cat ${LOCKFILE}`; then
+        echo "already running"
+        exit
+fi
+
+trap "rm -f ${LOCKFILE}; exit" INT TERM EXIT
+echo $$ > ${LOCKFILE}
+
+# Actual script
 while true
 do
 if ping -c 1 $SERVER > /dev/null;
@@ -22,3 +34,6 @@ if ping -c 1 $SERVER > /dev/null;
   fi
 sleep $SLEEP
 done
+
+#Remove lockfile
+rm -f ${LOCKFILE}
