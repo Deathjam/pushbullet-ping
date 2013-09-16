fcc9dd4005b1e47b8f06898c9b5264ba8903b55f
Phil <philwray@gmail.com>
2013-09-16 22:16:40 +0100

    Update pb-ping.sh

--- pb-ping.sh
+++ pb-ping.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Author: Phil Wray
 # Sign up at: https://www.pushbullet.com/
 # Enter your api and device_id below
 
@@ -9,14 +10,15 @@ API= #ACCOUNT API KEY
 DEV= #DEVICE ID
 HEADING="Houston we have a problem" #NOTE TITLE
 MESSAGE="Everything is on fire and the world is ending, oh noes" #NOTE BODY
+SLEEP=120 #Sleep time in seconds
 
 while true
 do
 if ping -c 1 $SERVER > /dev/null;
-    then echo "Everything is OK at $(date)"
+    then echo "Everything is fine at $(date)"
   else
-        curl -s $URL -u $API: -d device_id=$DEV -d type=note -d title=$HEADING -d body="$MESSAGE $(date)" -X POST
+        curl -s $URL -u $API: -d device_id=$DEV -d type=note -d title="$HEADING" -d body="$MESSAGE on $(date)" -X POST
     exit 0
   fi
-sleep 300
+sleep $SLEEP
 done
