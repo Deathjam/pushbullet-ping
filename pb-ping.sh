2d867ae649a9128e252182a167cd64a246da7760
Phil <philwray@gmail.com>
2015-01-16 12:02:07 +0000

    Update pb-ping.sh
    changes for new api

--- pb-ping.sh
+++ pb-ping.sh
@@ -6,11 +6,11 @@
 # Add script to cron to make sure always running
 
 SERVER=www.google.com #SERVER TO PING
-URL=https://www.pushbullet.com/api/pushes #PUSHBULLET API URL
+URL="https://api.pushbullet.com/v2/pushes" #PUSHBULLET API URL
 API= #ACCOUNT API KEY
-DEV= #DEVICE ID
-HEADING="Houston we have a problem" #NOTE TITLE
-MESSAGE="Everything is on fire and the world is ending, oh noes" #NOTE BODY
+
+NOTETITLE="Houston we have a problem" #NOTE TITLE
+NOTEBODY="Everything is on fire and the world is ending, oh noes" #NOTE BODY
 SLEEP=120 #Sleep time in seconds
 LOCKFILE=/tmp/pb-ping.lock
 
@@ -29,7 +29,12 @@ do
 if ping -c 1 $SERVER > /dev/null;
     then echo "Everything is fine at $(date)"
   else
-        curl -s $URL -u $API: -d device_id=$DEV -d type=note -d title="$HEADING" -d body="$MESSAGE on $(date)" -X POST
+          curl -u $API: -X POST $URL --header 'Content-Type: application/json' --data-binary '{
+                "type": "note",
+                "title":"'"$NOTETITLE"'",
+                "body": "'"$NOTEBODY"'"
+        }'
+        
     exit 0
   fi
 sleep $SLEEP
